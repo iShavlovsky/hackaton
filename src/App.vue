ad0710ebaaebd8f7df17603dd6823bdb2ad1afd3
Igor Shavlovsky <shahmanul34@gmail.com>
2024-05-30 12:42:55 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,25 @@
 </template>
 <script lang="ts" setup>
 import { type GlobalThemeOverrides, NConfigProvider } from 'naive-ui'
+import { useTitle } from '@vueuse/core'
 
+useTitle('Rev Share Party')
 const themeOverrides: GlobalThemeOverrides = {
-    // common: {
-    //     primaryColor: '#FF0000'
-    // },
-    // Button: {
-    //     textColor: '#FF0000'
-    // },
-    // Dropdown: {
-    //     color: '#FF0000'
-    // }
+    common: {
+        primaryColor: '#92FE75',
+        baseColor: '#92FE75'
+    },
+    Button: {
+        color: '#151518',
+        textColor: '#92FE75',
+        borderHover: '#8F75FE',
+        borderFocus: '#8F75FE'
+    },
+    Dropdown: {
+        color: '#151518',
+        optionTextColor: '#FFF',
+        optionColorHover: '#92FE75'
+    }
 }
 </script>
 <style scoped></style>
